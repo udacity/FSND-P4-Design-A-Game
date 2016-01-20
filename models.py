de619c48ee818b752eb54d26e965da6ce57f9822
cjmochrie <cameron.mochrie@gmail.com>
2016-01-20 10:04:54 -0500

    Add Score, ScoreForm models and endpoints. Update Game model to support.

--- models.py
+++ models.py
@@ -1,25 +1,31 @@
 import random
+from datetime import date
 from protorpc import messages
 from google.appengine.ext import ndb
 
-class Profile(ndb.Model):
+class User(ndb.Model):
     """User profile"""
-    pass
+    name = ndb.StringProperty(required=True)
 
 
 class Game(ndb.Model):
     """Game object"""
     target = ndb.IntegerProperty(required=True)
+    attempts_allowed = ndb.IntegerProperty(required=True)
     attempts_remaining = ndb.IntegerProperty(required=True, default=5)
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    user = ndb.KeyProperty(required=True, kind='User')
 
     @classmethod
-    def new_game(cls, min, max, attempts):
+    def new_game(cls, user, min, max, attempts):
         """Creates and returns a new game"""
-        print min, max, attempts
         if max < min:
             raise ValueError('Maximum must be greater than minimum')
-        game = Game(target=random.choice(range(1, max + 1)),
-                    attempts_remaining=attempts)
+        game = Game(user=user,
+                    target=random.choice(range(1, max + 1)),
+                    attempts_allowed=attempts,
+                    attempts_remaining=attempts,
+                    game_over=False)
         game.put()
         return game
 
@@ -27,32 +33,68 @@ class Game(ndb.Model):
         """Returns a GameForm representation of the Game"""
         form = GameForm()
         form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
         form.attempts_remaining = self.attempts_remaining
+        form.game_over = self.game_over
         form.message = message
         return form
 
+    def end_game(self, won=False):
+        """Ends the game - if won is True, the player won. - if won is False,
+        the player lost."""
+        self.game_over = True
+        self.put()
+        # Add the game to the score 'board'
+        score = Score(user=self.user, date=date.today(), won=won,
+                      guesses=self.attempts_allowed - self.attempts_remaining)
+        score.put()
 
 
 class Score(ndb.Model):
     """Score object"""
+    user = ndb.KeyProperty(required=True, kind='User')
+    date = ndb.DateProperty(required=True)
+    won = ndb.BooleanProperty(required=True)
+    guesses = ndb.IntegerProperty(required=True)
+
+    def to_form(self):
+        return ScoreForm(user_name=self.user.get().name, won=self.won,
+                         date=str(self.date), guesses=self.guesses)
 
 
 class GameForm(messages.Message):
     """GameForm for outbound game state information"""
     urlsafe_key = messages.StringField(1, required=True)
     attempts_remaining = messages.IntegerField(2, required=True)
-    message = messages.StringField(3, required=True)
+    game_over = messages.BooleanField(3, required=True)
+    message = messages.StringField(4, required=True)
+    user_name = messages.StringField(5, required=True)
 
 
 class NewGameForm(messages.Message):
     """Used to create a new game"""
-    min = messages.IntegerField(1, default=1)
-    max = messages.IntegerField(2, default=10)
-    attempts = messages.IntegerField(3, default=5)
+    user_name = messages.StringField(1, required=True)
+    min = messages.IntegerField(2, default=1)
+    max = messages.IntegerField(3, default=10)
+    attempts = messages.IntegerField(4, default=5)
+
 
 class MakeMoveForm(messages.Message):
     """Used to make a move in an existing game"""
     guess = messages.IntegerField(1, required=True)
+
+
+class ScoreForm(messages.Message):
+    """ScoreForm for outbound Score information"""
+    user_name = messages.StringField(1, required=True)
+    date = messages.StringField(2, required=True)
+    won = messages.BooleanField(3, required=True)
+    guesses = messages.IntegerField(4, required=True)
+
+
+class ScoreForms(messages.Message):
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(ScoreForm, 1, repeated=True)
 
 
 class StringMessage(messages.Message):
--- resources.py
+++ resources.py
@@ -1,8 +1,9 @@
 import endpoints
 from protorpc import remote, messages, message_types
 
-from models import Game
-from models import StringMessage, NewGameForm, GameForm, MakeMoveForm
+from models import User, Game, Score
+from models import StringMessage, NewGameForm, GameForm, MakeMoveForm, ScoreForm,\
+    ScoreForms
 from utils import get_by_urlsafe
 
 NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
@@ -13,6 +14,9 @@ GET_GAME_REQUEST = endpoints.ResourceContainer(
 MAKE_MOVE_REQUEST = endpoints.ResourceContainer(
     MakeMoveForm,
     urlsafe_game_key=messages.StringField(1),
+)
+USER_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1),
 )
 
 @endpoints.api(name='guess_a_number', version='v1')
@@ -25,7 +29,16 @@ class GuessANumberApi(remote.Service):
                       http_method='POST')
     def new_game(self, request):
         """Creates new game"""
-        game = Game.new_game(request.min, request.max, request.attempts)
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                    'A User with that name does not exist!')
+        try:
+            game = Game.new_game(user.key, request.min,
+                                 request.max, request.attempts)
+        except ValueError:
+            raise endpoints.BadRequestException('Maximum must be greater '
+                                                'than minimum!')
         return game.to_form('Good luck playing Guess a Number!')
 
     @endpoints.method(request_message=GET_GAME_REQUEST,
@@ -34,9 +47,8 @@ class GuessANumberApi(remote.Service):
                       name='get_game',
                       http_method='GET')
     def get_game(self, request):
-        """Return a current game state"""
+        """Return the current game state."""
         game = get_by_urlsafe(request.urlsafe_game_key, Game)
-
         if game:
             return game.to_form('Time to make a move!')
         else:
@@ -50,18 +62,61 @@ class GuessANumberApi(remote.Service):
     def  make_move(self, request):
         """Makes a move. Returns a game state with message"""
         game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game.game_over:
+            return game.to_form('Game already over!')
+
+        game.attempts_remaining -= 1
         if request.guess == game.target:
+            game.end_game(True)
             return game.to_form('You win!')
-        elif request.guess < game.target:
+
+        if request.guess < game.target:
             msg = 'Too low!'
         else:
             msg = 'Too high!'
-        game.attempts_remaining -= 1
-        game.put()
+
         if game.attempts_remaining < 1:
+            game.end_game(False)
             return game.to_form(msg + ' Game over!')
         else:
+            game.put()
             return game.to_form(msg)
+
+    @endpoints.method(response_message=ScoreForms,
+                      path='scores',
+                      name='get_scores',
+                      http_method='GET')
+    def get_scores(self, request):
+        """Return all scores"""
+        return ScoreForms(items=[score.to_form() for score in Score.query()])
+
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=ScoreForms,
+                      path='scores/user/{user_name}',
+                      http_method='GET')
+    def get_user_scores(self, request):
+        """Returns all of an individual User's scores"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                    'A User with that name does not exist!')
+        scores = Score.query(Score.user == user.key)
+        return ScoreForms(items=[score.to_form() for score in scores])
+
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=StringMessage,
+                      path='user',
+                      name='create_user',
+                      http_method='POST')
+    def create_user(self, request):
+        """Create a User. Requires a unique username"""
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                    'A User with that name already exists!')
+        user = User(name=request.user_name)
+        user.put()
+        return StringMessage(message='User {} created!'.format(
+                request.user_name))
 
 
 api = endpoints.api_server([GuessANumberApi])
